9bb03087f9b9bdfa6b1fe729234756376b0a5352
jaeleeman1 <fromjaelee@naver.com>
2017-12-17 22:56:58 +0900

    papa-stamp 디자인

--- routes/shop.js
+++ routes/shop.js
@@ -2,8 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('shop', { title: 'Express', title2: 'Express2' });
+router.get('/main', function(req, res, next) {
+  res.render('shop-main/shopMain', { title: 'Express', title2: 'Express2' });
+});
+
+router.get('/stamp', function(req, res, next) {
+    res.render('shop-stamp/stampMain', { title: 'Express', title2: 'Express2' });
+});
+
+router.get('/coupon', function(req, res, next) {
+    res.render('shop-coupon/couponList', { title: 'Express', title2: 'Express2' });
+});
+
+router.get('/map', function(req, res, next) {
+    res.render('shop-map/shopMap', { title: 'Express', title2: 'Express2' });
+});
+
+router.get('/setting', function(req, res, next) {
+    res.render('setting/setting', { title: 'Express', title2: 'Express2' });
 });
 
 module.exports = router;
