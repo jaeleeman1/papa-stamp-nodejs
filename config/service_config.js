5cc01f1e9950977a38be1463b1e8535bc2fe75a1
felix <fromjaelee@gmail.com>
2018-02-05 00:32:51 +0900

    Update service_config.js

--- config/service_config.js
+++ config/service_config.js
@@ -2,16 +2,16 @@
 var config = {};
 
 //Database
-config.host = '127.0.0.1';
-// config.host = 'papastamp.cgbapsrwofhr.ap-northeast-1.rds.amazonaws.com';
+//config.host = '127.0.0.1';
+config.host = 'papastamp.cgbapsrwofhr.ap-northeast-1.rds.amazonaws.com';
 config.port = 43306;
 config.username = 'ipark';
 config.password = 'lee2336^^';
 config.database = 'BEST_FOOD';
 
 //Domain
-config.url = 'http://localhost:8080';
-// config.url = 'https://whereareevent.com';
+//config.url = 'http://localhost:8080';
+config.url = 'https://whereareevent.com';
 
 //Logging
 config.loglevel = 'debug';
