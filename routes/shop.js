89c3d53254fb9123f5f0986eab65a27571bb2ee3
jaeleeman1 <fromjaelee@naver.com>
2018-03-04 19:11:54 +0900

    shop main 변경

--- routes/shop.js
+++ routes/shop.js
@@ -20,37 +20,35 @@ router.get('/main', function(req, res, next) {
         res.send('Invalid user id parameter error');
     }
 
+    var currentLat = req.query.current_lat;
+    var currentLng = req.query.current_lng;
+    logger.debug(TAG, 'Current latitude : ' + currentLat);
+    logger.debug(TAG, 'Current longitude : ' + currentLng);
+
+    if(currentLat == null || currentLat == undefined ||
+        currentLng == null || currentLng == undefined) {
+        logger.debug(TAG, 'Invalid location parameter error');
+        res.status(400);
+        res.send('Invalid location parameter error');
+    }
+
     getConnection(function (err, connection){
-        var selectUserLocationQuery = 'select SUI.CURRENT_LAT, SUI.CURRENT_LNG ' +
-            'from SB_USER_INFO as SUI ' +
-            'where SUI.USER_ID = ' + mysql.escape(userId);
-        connection.query(selectUserLocationQuery, function (err, userLocationData) {
+        var selectShopListMainQuery = 'select SSI.SHOP_ID, SSI.SHOP_FRONT_IMG, SSI.SHOP_BACK_IMG, ' +
+            '( 3959 * acos( cos( radians(' + mysql.escape(currentLat) + ') ) * cos( radians(SHOP_LAT) ) ' +
+            '* cos( radians(SHOP_LNG) - radians(' + mysql.escape(currentLng) + ') ) + sin( radians(' + mysql.escape(currentLat) + ') ) ' +
+            '* sin( radians(SHOP_LAT) ) ) ) AS distance ' +
+            'from SB_SHOP_INFO as SSI ' +
+            'having distance < 25 ' +
+            'order by distance limit 0, 10';
+        connection.query(selectShopListMainQuery, function (err, shopListMainData) {
             if (err) {
-                logger.error(TAG, "Select user location error : " + err);
+                logger.error(TAG, "Select shop list main error : " + err);
                 res.status(400);
-                res.send('Select user location error');
+                res.send('Select shop list main error');
             }else{
-                logger.debug(TAG, 'Select user location success : ' + JSON.stringify(userLocationData));
-                var currentLat = userLocationData[0].CURRENT_LAT;
-                var currentLng = userLocationData[0].CURRENT_LNG;
-                var selectShopListMainQuery = 'select SSI.SHOP_ID, SSI.SHOP_FRONT_IMG, SSI.SHOP_BACK_IMG, ' +
-                    '( 3959 * acos( cos( radians(' + mysql.escape(currentLat) + ') ) * cos( radians(SHOP_LAT) ) ' +
-                    '* cos( radians(SHOP_LNG) - radians(' + mysql.escape(currentLng) + ') ) + sin( radians(' + mysql.escape(currentLat) + ') ) ' +
-                    '* sin( radians(SHOP_LAT) ) ) ) AS distance ' +
-                    'from SB_SHOP_INFO as SSI ' +
-                    'having distance < 25 ' +
-                    'order by distance limit 0, 10';
-                connection.query(selectShopListMainQuery, function (err, shopListMainData) {
-                    if (err) {
-                        logger.error(TAG, "Select shop list main error : " + err);
-                        res.status(400);
-                        res.send('Select shop list main error');
-                    }else{
-                        logger.debug(TAG, 'Select shop list main success : ' + JSON.stringify(shopListMainData));
-                        res.status(200);
-                        res.render('common/papa-stamp', {view:'shop', url:config.url, userId:userId, shopId:'', shopListMainData:shopListMainData});
-                    }
-                });
+                logger.debug(TAG, 'Select shop list main success : ' + JSON.stringify(shopListMainData));
+                res.status(200);
+                res.render('common/papa-stamp', {view:'shop', url:config.url, userId:userId, shopId:'', shopListMainData:shopListMainData});
             }
             connection.release();
         });
